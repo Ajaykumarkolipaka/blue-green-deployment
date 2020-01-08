a5b109b49e888841dc72a7436d185cffe3eaf89a
Ajay <kolipaka02@gmail.com>
2020-01-08 20:50:19 +0530

    renaming the package in test folder

--- src/test/java/com/example/demo/cleanstrike/CleanStrikeMainTest.java
+++ src/test/java/com/example/demo/cleanstrike/CleanStrikeMainTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.sahaj;
+package com.example.demo.cleanstrike;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/test/java/com/example/demo/cleanstrike/CleanStrikeTediousTest.java
+++ src/test/java/com/example/demo/cleanstrike/CleanStrikeTediousTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.sahaj;
+package com.example.demo.cleanstrike;
 
 import static org.junit.Assert.assertEquals;
 
